82509dd3598e2c44ce208379d41b91778e88e368
ZhuRui <www.515246872@qq.com>
2021-03-22 14:21:51 +0800

    重定向构造函数

--- 04_Dart的面向对象/04_重定向构造函数.dart
+++ 04_Dart的面向对象/04_重定向构造函数.dart
@@ -9,10 +9,12 @@ class Person {
   String name;
   int age;
 
+  //  给age一个初始化值
   // Person(this.name) : age = 90;
 
   //  构造函数的重定向
+  //  给age一个初始化值；可以调用另一个函数
   Person(String name): this._internal(name, 70);
-
+  //  相当于上面的构造函数调用下面的构造函数，相互之间的调用
   Person._internal(this.name, this.age);
 }
